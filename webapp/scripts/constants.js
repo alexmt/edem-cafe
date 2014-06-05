d2bd58da293bd31a89174e3b355ba3c7500d8bbd
Alexander Matyushentsev <amatyushentsev@gmail.com>
2014-06-04 22:48:05 -0700

    Show full size img on sample click

--- webapp/scripts/constants.js
+++ webapp/scripts/constants.js
@@ -9,82 +9,82 @@ angular.module('constants')
   }).constant('menuInfo', { workSamples: [
     {
       thumbnailUrl: '399343037854_13.png',
-      fullUrl: '399343037854_0.png'
+      fullSizeUrl: '399343037854_0.png'
     },
     {
       thumbnailUrl: '399343038110_13.png',
-      fullUrl: '399343038110_0.png'
+      fullSizeUrl: '399343038110_0.png'
     },
     {
       thumbnailUrl: '399343038366_13.png',
-      fullUrl: '399343038366_0.png'
+      fullSizeUrl: '399343038366_0.png'
     },
     {
       thumbnailUrl: '399342576542_13.png',
-      fullUrl: '399342576542_0.png'
+      fullSizeUrl: '399342576542_0.png'
     },
     {
       thumbnailUrl: '399342577054_13.png',
-      fullUrl: '399342577054_0.png'
+      fullSizeUrl: '399342577054_0.png'
     },
     {
       thumbnailUrl: '399342577566_13.png',
-      fullUrl: '399342577566_0.png'
+      fullSizeUrl: '399342577566_0.png'
     },
     {
       thumbnailUrl: '399342578078_13.png',
-      fullUrl: '399342578078_0.png'
+      fullSizeUrl: '399342578078_0.png'
     },
     {
       thumbnailUrl: '399341977502_13.png',
-      fullUrl: '399341977502_0.png'
+      fullSizeUrl: '399341977502_0.png'
     },
     {
       thumbnailUrl: '399341977758_13.png',
-      fullUrl: '399341977758_0.png'
+      fullSizeUrl: '399341977758_0.png'
     },
     {
       thumbnailUrl: '399341978014_13.png',
-      fullUrl: '399341978014_0.png'
+      fullSizeUrl: '399341978014_0.png'
     },
     {
       thumbnailUrl: '399341978270_13.png',
-      fullUrl: '399341978270_0.png'
+      fullSizeUrl: '399341978270_0.png'
     },
     {
       thumbnailUrl: '399341978526_13.png',
-      fullUrl: '399341978526_0.png'
+      fullSizeUrl: '399341978526_0.png'
     },
     {
       thumbnailUrl: '399341978782_13.png',
-      fullUrl: '399341978782_0.png'
+      fullSizeUrl: '399341978782_0.png'
     },
     {
       thumbnailUrl: '399341979038_13.png',
-      fullUrl: '399341979038_0.png'
+      fullSizeUrl: '399341979038_0.png'
     },
     {
       thumbnailUrl: '399341979294_13.png',
-      fullUrl: '399341979294_0.png'
+      fullSizeUrl: '399341979294_0.png'
     },
     {
       thumbnailUrl: '399341979550_13.png',
-      fullUrl: '399341979550_0.png'
+      fullSizeUrl: '399341979550_0.png'
     },
     {
       thumbnailUrl: '399341979806_13.png',
-      fullUrl: '399341979806_0.png'
+      fullSizeUrl: '399341979806_0.png'
     },
     {
       thumbnailUrl: '399341980062_13.png',
-      fullUrl: '399341980062_0.png'
+      fullSizeUrl: '399341980062_0.png'
     },
     {
       thumbnailUrl: '399341980318_13.png',
-      fullUrl: '399341980318_0.png'
+      fullSizeUrl: '399341980318_0.png'
     },
     {
       thumbnailUrl: '399341980574_13.png',
-      fullUrl: '399341980574_0.png'
+      fullSizeUrl: '399341980574_0.png'
     }
   ]});
--- webapp/scripts/controllers.js
+++ webapp/scripts/controllers.js
@@ -4,5 +4,13 @@ angular.module('controllers').controller('MainCtrl', ['$scope', 'legalEntityInfo
   function ($scope, legalEntityInfo) {
     $scope.info = legalEntityInfo;
   }]).controller('SamplesCtrl', ['$scope', 'menuInfo', function ($scope, menuInfo) {
+  $scope.isFullSizeVisible = false;
   $scope.samples = menuInfo.workSamples;
+  $scope.showFullSizeImage = function (url) {
+    $scope.fullSizeUrl = url;
+    $scope.isFullSizeVisible = true;
+  };
+  $scope.hideFullSizeImage = function() {
+    $scope.isFullSizeVisible = false;
+  }
 }]);
